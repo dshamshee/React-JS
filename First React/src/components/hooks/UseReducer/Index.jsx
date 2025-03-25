8fc579ccb78e8e89ee6abe1adeaaec159933b4e4
Danish <danishshamshee123@gmail.com>
2025-03-25 23:18:27 +0530

    learn how to use useReducer Hook in complex project

--- First React/src/components/hooks/UseReducer/Index.jsx
+++ First React/src/components/hooks/UseReducer/Index.jsx
@@ -1,27 +1,53 @@
 import { useReducer } from "react"
 
-export const ReducerComp = ()=>{
+export const ReducerComp = () => {
 
-    const reducer = (state, action)=>{
-        console.log(state, action)
-        if(action.type === "INCREAMENT"){
-            return state +1 ;
-        }
+    const initialState = {
+        count: 0,
+        inc: 2,
+        dec: 2
+    }
 
-        if(action.type === "DECREAMENT"){
-            return state - 1 ;
+    const reducer = (state, action) => {
+        // console.log(state, action)
+        // if(action.type === "INCREAMENT"){
+        //     return state +1 ;
+        // }
+
+        // if(action.type === "DECREAMENT"){
+        //     return state - 1 ;
+        // }
+
+        // if(action.type === 'RESET'){
+        //     return state = 0;
+        // }
+
+
+        switch (action.type) {
+            case 'INCREAMENT':
+                return {...state, count: state.count + state.inc}
+
+            case 'DECREAMENT':
+                return {...state, count: state.count - state.dec}
+
+            case 'RESET':
+                return {...state, count: 0}
+
+
+            default: return state;
         }
     }
 
 
-    const [count, dispatch] = useReducer(reducer, 0)
+    const [state, dispatch] = useReducer(reducer, initialState)
     // console.log(useReducer())
-    return(
+    return (
         <>
             <div className="p-4 h-lvh flex flex-col justify-center items-center">
-                <h1>{count}</h1>
-                <button onClick={()=> dispatch({type: "INCREAMENT"})}>Increament</button>
-                <button onClick={()=> dispatch({type: "DECREAMENT"})}>Decreament</button>
+                <h1>{state.count}</h1>
+                <button onClick={() => dispatch({ type: "INCREAMENT" })}>Increament</button>
+                <button onClick={() => dispatch({ type: "DECREAMENT" })}>Decreament</button>
+                <button onClick={() => dispatch({ type: "RESET" })}>Reset</button>
             </div>
         </>
     )
