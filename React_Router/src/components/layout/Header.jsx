912e43622cf35f30ed7964ee7985aa4f32758ffc
Danish <danishshamshee123@gmail.com>
2025-04-02 00:04:32 +0530

    create Header & Footer

--- React_Router/src/components/layout/Header.jsx
+++ React_Router/src/components/layout/Header.jsx
@@ -1,4 +1,46 @@
+import { NavLink } from "react-router-dom"
 
-export const Header = ()=>{
-    return <h1>Header</h1>
+export const Header = () => {
+    return (
+        <>
+            <header className="section-navbar">
+                <section className="top_txt">
+                    <div className="head container">
+                        <div className="head_txt">
+                            <p>get Danish Membership, 30 day return or refund guarantee</p>
+                        </div>
+                        <div className="sign_in_up">
+                            <NavLink to="">SIGN IN</NavLink>
+                            <NavLink to="">SIGN UP</NavLink>
+                        </div>
+                    </div>
+                </section>
+
+                <div className="container">
+                    <div className="navbar-brand">
+                        <NavLink to="index">
+                            <p>DFlix</p>
+                        </NavLink>
+                    </div>
+
+                    <nav className="navbar">
+                        <ul>
+                            <li className="nav-item">
+                                <NavLink to="/" className="nav-link">Home</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to="/about" className="nav-link">About</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to="/movie" className="nav-link">Movies</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to="/contact" className="nav-link">contact</NavLink>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </header>
+        </>
+    )
 }
