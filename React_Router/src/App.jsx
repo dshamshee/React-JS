fe605d6f5608c1c23dab1ec3b400cc65c26b37b0
Danish <danishshamshee123@gmail.com>
2025-04-01 22:28:09 +0530

    learn Create Route using traditional method

--- React_Router/src/App.jsx
+++ React_Router/src/App.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
 import { Movie } from "./pages/Movie";
@@ -6,24 +6,35 @@ import { Contact } from "./pages/Contact";
 
 const App = () => {
 
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Home />
-    },
-    {
-      path: "/about",
-      element: <About />
-    },
-    {
-      path: "/movie",
-      element: <Movie />
-    },
-    {
-      path: "/contact",
-      element: <Contact />
-    }
-  ])
+  // const router = createBrowserRouter([
+  //   {
+  //     path: "/",
+  //     element: <Home />
+  //   },
+  //   {
+  //     path: "/about",
+  //     element: <About />
+  //   },
+  //   {
+  //     path: "/movie",
+  //     element: <Movie />
+  //   },
+  //   {
+  //     path: "/contact",
+  //     element: <Contact />
+  //   }
+  // ])
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/movie" element={<Movie />} />
+        <Route path="/contact" element={<Contact />} />
+      </Route>
+    )
+  )
 
 
   return <RouterProvider router={router} />
